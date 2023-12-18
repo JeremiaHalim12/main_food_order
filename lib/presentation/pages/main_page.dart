7a5be0cbf67fd2eac29bd453ca661827c03acf64
JeremiaHalim <2072021@maranatha.ac.id>
2023-12-19 01:46:27 +0700

    updated

--- lib/presentation/pages/main_page.dart
+++ lib/presentation/pages/main_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:main_food_order/data/repositories/kategori_repository.dart';
+import 'package:main_food_order/domain/usecase/get_kategori_usecase.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -19,7 +21,7 @@ class _MainPageState extends State<MainPage> {
         child: Padding(
           padding: const EdgeInsets.all(20.0),
           child: Center(
-            child: Text('data'),
+            child: Text('data')
           ),
         ),
       ),
